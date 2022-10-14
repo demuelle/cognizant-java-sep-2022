f5641086c5265315e70eadfd818824d30937dcb6
demuelle <dmueller@trilogyed.com>
2022-10-14 09:04:51 -0500

    Removes album mock so we can practice in class

--- week-3/day-3/record-collection/src/test/java/com/company/reccoll/service/ServiceLayerTest.java
+++ week-3/day-3/record-collection/src/test/java/com/company/reccoll/service/ServiceLayerTest.java
@@ -43,27 +43,6 @@ public class ServiceLayerTest {
 
     private void setUpAlbumRepositoryMock() {
         albumRepository = mock(AlbumRepository.class);
-        Album album = new Album();
-        album.setId(1);
-        album.setArtistId(45);
-        album.setLabelId(10);
-        album.setTitle("Greatest Hits");
-        album.setListPrice(new BigDecimal("14.99"));
-        album.setReleaseDate(LocalDate.of(1999, 05, 15));
-
-        Album album2 = new Album();
-        album2.setArtistId(45);
-        album2.setLabelId(10);
-        album2.setTitle("Greatest Hits");
-        album2.setListPrice(new BigDecimal("14.99"));
-        album2.setReleaseDate(LocalDate.of(1999, 05, 15));
-
-        List<Album> aList = new ArrayList<>();
-        aList.add(album);
-
-        doReturn(album).when(albumRepository).save(album2);
-        doReturn(Optional.of(album)).when(albumRepository).findById(1);
-        doReturn(aList).when(albumRepository).findAll();
     }
 
     private void setUpArtistRepositoryMock() {
